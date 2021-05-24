deba95c189e5f6b99ad08fad10af28e656480068
wormtooth <ye@wormtooth.com>
2021-05-24 10:58:10 -0400

    fix paform

--- forms/paform.py
+++ forms/paform.py
@@ -1,11 +1,9 @@
-import utils
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, SelectField, SubmitField
 from wtforms.validators import Required
 
-df = utils.load_tables(utils.DATA_FOLDER)['full']
-PAYERS = df.bin.unique().tolist()
-DRUGS = df.drug.unique().tolist()
+PAYERS = ['417380', '999001', '417740', '417614']
+DRUGS = ['A', 'B', 'C']
 
 
 class PriorAuthForm(FlaskForm):
