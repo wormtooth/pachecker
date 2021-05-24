ca6137ea01e1c55f5d7065509034fa8708081ba0
wormtooth <ye@wormtooth.com>
2021-05-24 09:28:19 -0400

    cache formulary

--- models/formulary.py
+++ models/formulary.py
@@ -1,17 +1,45 @@
+import json
+import os
+
 import utils
 
-FORMULARY = None
+
+def _flatten(formulary):
+    return [
+        {'key': key, 'val': val}
+        for key, val in formulary.items()
+    ]
+
+
+def _recover(formulary):
+    return {
+        tuple(item['key']): item['val']
+        for item in formulary
+    }
 
 
 def get_formulary():
-    global FORMULARY
-    if FORMULARY:
-        return FORMULARY
-    FORMULARY = {}
+
+    cache_path = os.path.join(utils.APP_FOLDER, 'models', 'formulary.json')
+    formulary = None
+    if os.path.exists(cache_path):
+        try:
+            with open(cache_path, 'r') as f:
+                formulary = json.load(f)
+        except Exception:
+            pass
+
+    if formulary:
+        return _recover(formulary)
+
+    formulary = {}
     folder = utils.DATA_FOLDER
     df = utils.load_tables(folder)['dim_claims']
     for payer, drug, code in df.groupby(['bin', 'drug', 'reject_code']).size().index:
         if code == 0:
             continue
-        FORMULARY[payer, drug] = int(code)
-    return FORMULARY
+        formulary[payer, drug] = int(code)
+
+    with open(cache_path, 'w') as f:
+        json.dump(_flatten(formulary), f)
+    return formulary
