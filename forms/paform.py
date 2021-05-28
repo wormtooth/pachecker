9410883f3da4d7c75738077381c363ede279e2da
wormtooth <ye@wormtooth.com>
2021-05-27 20:01:27 -0400

    add help text

--- forms/paform.py
+++ forms/paform.py
@@ -7,11 +7,29 @@ DRUGS = ['A', 'B', 'C']
 
 
 class PriorAuthForm(FlaskForm):
-    payer = SelectField('Payer', choices=PAYERS, validators=[Required()])
-    drug = SelectField('Drug', choices=DRUGS, validators=[Required()])
+    payer = SelectField(
+        'Payer',
+        description="select the patient's insurance company",
+        choices=PAYERS, validators=[Required()]
+    )
+    drug = SelectField(
+        'Drug',
+        description="select the drug for the patient",
+        choices=DRUGS,
+        validators=[Required()]
+    )
 
-    correct_diagnosis = BooleanField('correct diagnosis')
-    tried_and_failed = BooleanField('tried and failed')
-    contraindication = BooleanField('contraindication')
+    correct_diagnosis = BooleanField(
+        'correct diagnosis',
+        description="check if the patient has the correct diagnosis for the associated drug"
+    )
+    tried_and_failed = BooleanField(
+        'tried and failed',
+        description="check if the patient has tried and failed the relevant generic alternatives"
+    )
+    contraindication = BooleanField(
+        'contraindication',
+        description="check if the patient has an associated contraindication for the medication requested"
+    )
 
     submit = SubmitField('Check!')
